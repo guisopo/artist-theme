36689cc1ca7bb8121cc037b625a513f5c625fb30
guisopo <solerguillermo@hotmail.com>
2019-06-13 19:35:15 +0200

    Add title and description to the head

--- header.php
+++ header.php
@@ -16,6 +16,8 @@
   <meta charset="<?php bloginfo( 'charset' ); ?>">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title><?php bloginfo( 'name' ); wp_title(); ?></title>
+  <meta name="description" content="<?php bloginfo( 'description' ); ?>">
   <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
