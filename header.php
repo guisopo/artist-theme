db717c9c7575381c649877d6b94f941b2cabf47b
guisopo <solerguillermo@hotmail.com>
2019-06-14 14:15:49 +0200

    Simple clean up

--- header.php
+++ header.php
@@ -14,10 +14,12 @@
 <html <?php language_attributes(); ?>>
 <head>
   <meta charset="<?php bloginfo( 'charset' ); ?>">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <meta name="description" content="<?php bloginfo( 'description' ); ?>">
+  <link rel="profile" href="http://gmpg.org/xfn/11">
+
   <?php wp_head(); ?>
 </head>
+
 <body <?php body_class(); ?>>
   <div class="content">
