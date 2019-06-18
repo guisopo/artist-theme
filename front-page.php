543d06ce9e4b50d8fcfc01456fe86dac666decb0
guisopo <solerguillermo@hotmail.com>
2019-06-18 14:29:47 +0200

    Add header, heading title and footer

---
+++ front-page.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Front Page
+ *
+ * This is used to render your site’s front page.
+ * 
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/#front-page-display
+ *
+ * @package Artist
+ */
+
+get_header(); 
+?>
+
+<h1>Hola!</h1>
+
+<?php 
+get_footer();
