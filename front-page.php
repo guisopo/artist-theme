588a7a8a8d7eb08ea6092c911fcb7dbf5e5ed1d0
guisopo <solerguillermo@hotmail.com>
2019-06-18 20:44:07 +0200

    Add arguments to retrieve just parent elements and exlude the Uncategorized category

--- front-page.php
+++ front-page.php
@@ -17,16 +17,17 @@ get_header();
 <?php
   $categories = get_categories( array(
     'orderby' => 'name',
-    'parent'  => 0
+    'parent'  => 0,
+    'exclude' => '1'
 ) );
  
 foreach ( $categories as $category ) {
-  if( $category->name !== 'Uncategorized' ) {
+
     printf( '<a href="%1$s">%2$s</a><br />',
         esc_url( get_category_link( $category->term_id ) ),
         esc_html( $category->name )
     );
-  }
+
 }
   
 ?>
