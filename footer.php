56b25e0e6ad6f66d8afeb5db9aeaf20163715671
guisopo <solerguillermo@hotmail.com>
2019-06-19 00:01:17 +0200

    Add copyright in footer

--- footer.php
+++ footer.php
@@ -10,9 +10,15 @@
  */
 ?>
 
-	</div><!-- #content -->
+		</div><!-- #content -->
 
-<?php wp_footer(); ?>
+		<footer class="site-footer section-inner">
 
-</body>
+			<p>&copy; <?php echo date( 'Y' ); ?> <a href="<?php echo esc_url( home_url() ); ?>" class="site-name"><?php bloginfo( 'name' ); ?></a></p>
+
+		</footer> <!-- footer -->
+		
+		<?php wp_footer(); ?>
+
+	</body>
 </html>
