061c03d220cbecbc5b05e44a136a910a4f0e6dcb
guisopo <solerguillermo@hotmail.com>
2019-06-20 10:04:36 +0200

    Add basic 404 markup

---
+++ 404.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * The template for displaying 404 pages (not found)
+ *
+ * @link https://codex.wordpress.org/Creating_an_Error_404_Page
+ *
+ * @package Artist
+ */
+
+get_header(); 
+?>
+
+<h1><?php echo '404!<br/>That page can&rsquo;t be found.'; ?></h1>
+
+<?php 
+get_footer();
+
